79ed5e4d52204c88965e3c9991abd2f815cb8b14
OhenebaAduhene <aduhenewisdom97@gmail.com>
2020-06-20 21:38:08 +0000

    db

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 	//variables for DB connections 
-	define("DB_NAME", "pax" );
+	define("DB_NAME", "heroku_dce5363aa56d888" );
 	define("DB_PWD", "e6a094fb" );
 	define("DB_HOST", "us-cdbr-east-05.cleardb.net" );
 	define("DB_USER", "be82219c9c49e7");
