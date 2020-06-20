87aac43a4a2e423bc3865d376b4db1e9ac68e977
OhenebaAduhene <aduhenewisdom97@gmail.com>
2020-06-20 21:31:19 +0000

    heroku update

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 	//variables for DB connections 
 	define("DB_NAME", "pax" );
 	define("DB_PWD", "e6a094fb" );
-	define("DB_HOST", "localhost" );
+	define("DB_HOST", "us-cdbr-east-05.cleardb.net" );
 	define("DB_USER", "be82219c9c49e7");
 
 ?>
